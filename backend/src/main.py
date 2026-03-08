745f7bab25ea16d3c86787455c9fd9ffd39b5ce4
Starfury-gh <vladimirel08@mail.ru>
2026-03-08 17:03:32 +0300

    backend: fixed images dir error. Fixed Dockerfile

--- backend/src/main.py
+++ backend/src/main.py
@@ -7,6 +7,8 @@ from api.hero import h_router
 from api.award import a_router
 from api.rank import r_router
 
+from os import path, makedirs
+
 app = FastAPI()
 
 app.include_router(h_router)
@@ -23,12 +25,15 @@ app.add_middleware(
     allow_methods=["*"]
 )
 
+if not path.exists("images"):
+    makedirs("images")
+
 app.mount("/images", StaticFiles(directory="images"), name="images")
 
 if __name__ == "__main__":
     run(
         "main:app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=8000,
         reload=True
     )
