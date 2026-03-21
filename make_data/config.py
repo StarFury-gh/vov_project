fbe0e141557740b712255a73f963c43d461e2690
Starfury-gh <vladimirel08@mail.ru>
2026-03-21 10:06:55 +0300

    make_data: minor changes

--- make_data/config.py
+++ make_data/config.py
@@ -3,6 +3,8 @@ class Config:
         self.NAMES_FILE = "names.txt"
         self.JSON_FILE = "data.json"
         self.JSON_FILE_SVO = "data_svo.json"
+        # self.JSON_FILE = "data_test.json"
+        # self.JSON_FILE_SVO = "data_test.json"
         self.API_URL = "http://127.0.0.1:8000"
 
 config_obj = Config()
