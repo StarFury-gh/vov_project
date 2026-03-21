a962df9f3094b214f076e90702fb61865b794b93
Starfury-gh <vladimirel08@mail.ru>
2026-03-21 15:14:46 +0300

    postgres: removed users table

--- postgres/init.sql
+++ postgres/init.sql
@@ -19,16 +19,6 @@ CREATE TABLE awards (
 
 CREATE INDEX idx_awards_name ON awards(name);
 
--- Таблица пользователей (для административной панели)
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    is_active BOOLEAN DEFAULT TRUE
-);
-
-CREATE INDEX idx_users_username ON users(username);
-
 -- Основная таблица героев
 CREATE TABLE heroes (
     id SERIAL PRIMARY KEY,
