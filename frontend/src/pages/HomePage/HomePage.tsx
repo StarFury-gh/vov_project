a94f088c0c50ae6cff4b9aa5a069f62fb642c03a
Starfury-gh <vladimirel08@mail.ru>
2026-04-01 20:00:36 +0300

    frontend: added interactivity to navigation cards on the main page

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,16 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import styles from './HomePage.module.css';
 
 function HomePage() {
+
+    const navigate = useNavigate()
+
     return (
         <div className={styles['home-page']}>
             <h1 className={styles['home-page__title']}>Справочник героев СВО и ВОВ</h1>
-            {/* Основной контент */}
             <section>
-                <div className={styles['home-page__content-block']}>
-                    {/* Подгрузим позже */}
+                <div className={styles['home-page__content-block']} onClick={() => navigate("/about")}>
                     <img
                         src="/docs.jpeg"
                         alt="/docs.jpeg"
@@ -18,40 +19,36 @@ function HomePage() {
                     <div className={styles['home-page__content-block-text']}>
                         <h2 className={styles['home-page__content-block-title']}>Цель проекта</h2>
                         <p className={styles['home-page__content-block-description']}>«Никто не забыт, ничто не забыто» — для нас это не просто строка из стихотворения, а ежедневный труд.</p>
-                        <Link to="/about">Подробнее</Link>
+                        <Link className={styles["link"]} to="/about">Подробнее</Link>
                     </div>
                 </div>
-                <div className={styles['home-page__content-block']}>
-                    {/* Подгрузим позже */}
+                <div className={styles['home-page__content-block']} onClick={() => navigate("/vov")}>
                     <img src="/vov.jpeg" alt="" className={styles['home-page__content-block-img']} />
                     <div className={styles['home-page__content-block-text']}>
                         <h2 className={styles['home-page__content-block-title']}>Инфомарция о Героях Великой Отечественной Войны</h2>
-                        <Link to="/vov">Перейти</Link>
+                        <Link className={styles["link"]} to="/vov">Перейти</Link>
                     </div>
                 </div>
-                <div className={styles['home-page__content-block']}>
-                    {/* Подгрузим позже */}
+                <div className={styles['home-page__content-block']} onClick={() => navigate("/svo")}>
                     <img src="/svo.jpeg" alt="" className={styles['home-page__content-block-img']} />
                     <div className={styles['home-page__content-block-text']}>
                         <h2 className={styles['home-page__content-block-title']}>Информация о Героях Специальной Военной Операции</h2>
-                        <Link to="/svo">Перейти</Link>
+                        <Link className={styles["link"]} to="/svo">Перейти</Link>
                     </div>
                 </div>
-                <div className={styles['home-page__content-block']}>
-                    {/* Подгрузим позже */}
+                <div className={styles['home-page__content-block']} onClick={() => navigate("/about")}>
                     <img src="/lenta.jpeg" alt="" className={styles['home-page__content-block-img']} />
                     <div className={styles['home-page__content-block-text']}>
                         <h2 className={styles['home-page__content-block-title']}>О нас</h2>
                         <p className={styles['home-page__content-block-description']}>Читайте подробнее</p>
-                        <Link to={"/about"}>Читать</Link>
+                        <Link className={styles["link"]} to={"/about"}>Читать</Link>
                     </div>
                 </div>
-                <div className={styles['home-page__content-block']}>
-                    {/* Подгрузим позже */}
+                <div className={styles['home-page__content-block']} onClick={() => navigate("/add")}>
                     <img src="/lenta.jpeg" alt="" className={styles['home-page__content-block-img']} />
                     <div className={styles['home-page__content-block-text']}>
                         <h2 className={styles['home-page__content-block-title']}>Вы можете добавить информацию о герое, которого нет на нашем сайте</h2>
-                        <Link to={"/add"}>Добавить</Link>
+                        <Link className={styles["link"]} to={"/add"}>Добавить</Link>
                     </div>
                 </div>
             </section>
